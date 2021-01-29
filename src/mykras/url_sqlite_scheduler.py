6c69d1b80a6b3b0dc4cb9a2f9030a7b25b1898f2
Igor Krasovskiy <i.krasovskiy@gmail.com>
2021-01-29 17:06:32 +0200

    Add file.

--- src/mykras/url_sqlite_scheduler.py
+++ src/mykras/url_sqlite_scheduler.py
@@ -1,15 +1,24 @@
-from requests
+import requests
 import json
 from collections import namedtuple
 from contextlib import closing
 import sqlite3
+import datetime
 
 from prefect import task, Flow
+from prefect.tasks.database.sqlite import SQLiteScript
+from prefect.schedules import IntervalSchedule
+
+## setup
+create_table = SQLiteScript(
+    db='cfpbcomplaints.db',
+    script='CREATE TABLE IF NOT EXISTS complaint (timestamp TEXT, state TEXT, product TEXT, company TEXT, complaint_what_happened TEXT)'
+)
 
 ## extract
 @task
 def get_complain_data():
-    r = requests.get('https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/', params={'size':10})
+    r = requests.get('https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/', params={'size': 10})
     response_json = json.loads(r.text)
     return response_json['hits']['hits']
 
@@ -18,7 +27,7 @@ def get_complain_data():
 def parse_complaint_data(raw):
     complaints = []
     Complaint = namedtuple('Complaint', ['data_received', 'state', 'product', 'company', 'complaint_what_happened'])
-    for row in row:
+    for row in raw:
         source = row.get('_source')
         this_complaint = Complaint(
             data_received=source.get('data_received'),
@@ -30,22 +39,26 @@ def parse_complaint_data(raw):
         complaints.append(this_complaint)
         return complaints
 
-
 ## load
 @task
 def store_complaints(parsed):
-    create_script = 'CREATE_TABLE_IF_NOT_EXIST complaint(timestamp TEXT, state TEXT, product TEXT, company TEXT, complaint_what_happened TEXT)'
-    insert_cmd = 'INSERT INTO complain VALUES (?. ?. ?. ?. ?)'
+    #create_script = 'CREATE_TABLE_IF_NOT_EXISTS complaint (timestamp TEXT, state TEXT, product TEXT, company TEXT, complaint_what_happened TEXT)'
+    insert_cmd = 'INSERT INTO complaint VALUES (?, ?, ?, ?, ?)'
 
-    with closing(sqlite3.connect('cfpbcomplaints')) as conn:
+    with closing(sqlite3.connect('cfpbcomplaints.db')) as conn:
         with closing(conn.cursor()) as cursor:
-            cursor.executescript(create_script)
+            #cursor.executescript(create_script)
             cursor.executemany(insert_cmd, parsed)
             conn.commit()
 
+schedule = IntervalSchedule(interval=datetime.timedelta(minutes=1))
+
+
 with Flow('my etl flow') as f:
+    db_table = create_table()
     raw = get_complain_data()
     parsed = parse_complaint_data(raw)
-    store_complaints(parsed)
+    populated_table = store_complaints(parsed)
+    populated_table.set_upstream(db_table)
 
 f.run()
